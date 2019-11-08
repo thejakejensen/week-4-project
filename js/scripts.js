7f5b8bf9d3f098f0d733f0d9c23a43a9e69e2ecf
Jacob Jensen <thejakejensen@gmail.com>
2019-11-08 11:49:10 -0800

    protype to add price based on size

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,16 @@ function Pizza(pizzaSize, cheese) {
   this.pizzaSize = pizzaSize;
   this.cheese = cheese;
   this.meatToppings = [];
-  this.vegToppings = [];'`  `
+  this.vegToppings = [];
+}
+Pizza.prototype.addCost = function() {
+  var cost = 0;
+  if (this.pizzaSize === "medium") {
+    cost = 5;
+  } else if (this.pizzaSize === "small") {
+    cost = 3;
+  } else if (this.pizzaSize === "large") {
+    cost = 8;
+  } else if (this.pizzaSize === "extra large") {
+    cost = 10;
+  }
